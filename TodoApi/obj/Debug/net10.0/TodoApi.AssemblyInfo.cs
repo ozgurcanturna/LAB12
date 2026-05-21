aa5bb49a18b47f77013a907e3076a4b2c9c25747
ozgurcan.turna@iuc.edu.tr <ozgurcan.turna@iuc.edu.tr>
2026-05-21 10:58:58 +0300

    açıklama sildim.

--- TodoApi/obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32cdb4e563865b7c3371869455c6796abeab523f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e1391d3843c421dbce9698367b3d8550e5fe0c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
